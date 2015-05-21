52aec87406c81ed658d2003dba93fb33506f44ca
Benjamin J. Anderson <andeb2804@gmail.com>
2015-05-21 17:28:07 -0400

    Restructure directories for session lib

--- rime/actiondispatch/load.hh
+++ rime/actiondispatch/load.hh
@@ -1,13 +1,14 @@
 <?hh // strict
 
-require_once 'src/exception/Exception.hh';
-require_once 'src/exception/RouteNotFound.hh';
-require_once 'src/exception/UnexpectedValue.hh';
-require_once 'src/spec/AbstractSpec.hh';
-require_once 'src/route/RouteGenerator.hh';
-require_once 'src/regex/Regex.hh';
-require_once 'src/route/Route.hh';
-require_once 'src/route/RouteCollection.hh';
-require_once 'src/factory/RouteFactory.hh';
-require_once 'src/route/Router.hh';
-require_once 'src/factory/RouterFactory.hh';
+require_once 'src/dispatcher/exception/Exception.hh';
+require_once 'src/dispatcher/exception/RouteNotFound.hh';
+require_once 'src/dispatcher/exception/UnexpectedValue.hh';
+require_once 'src/dispatcher/spec/AbstractSpec.hh';
+require_once 'src/dispatcher/route/RouteGenerator.hh';
+require_once 'src/dispatcher/regex/Regex.hh';
+require_once 'src/dispatcher/route/Route.hh';
+require_once 'src/dispatcher/route/RouteCollection.hh';
+require_once 'src/dispatcher/factory/RouteFactory.hh';
+require_once 'src/dispatcher/route/Router.hh';
+require_once 'src/dispatcher/factory/RouterFactory.hh';
+ 
