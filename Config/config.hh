9c732a90b9f45f080c755f8f56333814bf0da68e
Benjamin J. Anderson <andeb2804@gmail.com>
2015-05-22 16:36:27 -0400

    Set up basic config layout

--- Config/config.hh
+++ Config/config.hh
@@ -1,3 +1,5 @@
 <?hh // strict
-
+  
 require_once 'constants.hh';
+require_once 'autoload.hh';
+require_once 'routes.hh';
--- Config/constants.hh
+++ Config/constants.hh
@@ -1,3 +1,17 @@
 <?hh // strict
 
+/*
+ * Minimum HHVM version required to run Rime
+ * 380 translates to 3.8.0 (-dev if using a nightly build)
+ * Run the dependencyTest in Test/dependencies/dependencyTest.hh to verify your HHVM version
+ * ex: hhvm /usr/bin/phpunit --testsuite dependencies
+ */
 define('HHVM_MIN_VERSION',380);
+
+/*
+ * Base Path relative to public_html/index.hh
+ * BASE_PATH provides the fundamental basis to autoloading classes
+ * used in the Rime namespace as well as user-defined controllers, models,
+ * and views.
+ */
+define('BASE_PATH',dirname(dirname(__FILE__)));
