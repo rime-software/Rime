97784a7c59f5cb24808b0c460802d768e0e004ad
Benjamin J. Anderson <ben@rimesystems.com>
2015-07-30 23:21:35 -0500

    Update Rime.hh

--- Rime/System/Framework/Rime.hh
+++ Rime/System/Framework/Rime.hh
@@ -7,6 +7,16 @@ class Rime extends \Rime\System\Implementation\Singleton
   
   private Map $attachments = Map{};
   
+  public static function filter_get(string $key, $filter_type = false): mixed
+  {
+    return filter_input(INPUT_GET, $key, $filter_type ? $filter_type : FILTER_DEFAULT);  
+  }
+
+  public static function filter_post(string $key, $filter_type = false): mixed
+  {
+    return filter_input(INPUT_POST, $key, $filter_type ? $filter_type : FILTER_DEFAULT);  
+  }
+  
   public function attach(Mixed $element, string $alias): void
   {
     $this->attachments[$alias] = $element;
